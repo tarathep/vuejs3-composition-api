8794a32cdeb6091a64b7b373a5639f74fa2d2d4d
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2023-11-07 17:52:15 +0700

    Extracting Complexity

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,58 +1,20 @@
 <script setup lang="ts">
-import { DateTime } from "luxon"
-import { ref, computed } from "vue"
-import { TimeLinePost } from "./../posts"
 import { usePosts } from "../stores/posts";
 import TimelineItem from "./TimelineItem.vue";
+import { periods } from "../constants"
 
 const postsStore = usePosts()
-
-const periods = ["Today", "This Week", "This Month"] as const
-
-type Period = typeof periods[number]
-
-const selectedPeriod = ref<Period>("Today")
-
-function selectPeriod(period: Period) {
-    selectedPeriod.value = period
-}
-
-const posts = computed<TimeLinePost[]>(() => {
-    return postsStore.ids
-        .map(id => {
-            const post = postsStore.all.get(id)!
-            if (!post) {
-                throw Error(`Post with id of ${id} was expected but not found.`)
-            }
-            return {
-                ...post,
-                created: DateTime.fromISO(post.created)
-            }
-        })
-        .filter(post => {
-            if (selectedPeriod.value === "Today") {
-                return post.created >= DateTime.now().minus({ day: 1 })
-            }
-
-            if (selectedPeriod.value === "This Week") {
-                return post.created >= DateTime.now().minus({ week: 1 })
-            }
-            return post
-        })
-})
 </script>
 
 <template>
     <nav class="is-primary panel">
         <span class="panel-tabs">
-            <a v-for="period of periods" :key="period" :class="{ 'is-active': period === selectedPeriod }"
-                @click="selectPeriod(period)">
+            <a v-for="period of periods" :key="period" :class="{ 'is-active': period === postsStore.selectedPeriod }"
+                @click="postsStore.setSelectedPeriod(period)">
                 {{ period }}
             </a>
         </span>
 
-        <TimelineItem v-for="post of posts" :key="post.id" :post="post" />
-
-
+        <TimelineItem v-for="post of postsStore.fillteredPosts" :key="post.id" :post="post" />
     </nav>
 </template>
--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -1,23 +1,54 @@
-import { defineStore } from "pinia"
-import { Post, today, thisWeek, thisMonth } from "../posts";
+import { defineStore } from "pinia";
+import { Period } from "../constants";
+import { Post, today, thisWeek, thisMonth, TimeLinePost } from "../posts";
+import { DateTime } from "luxon";
 
 interface PostsState {
-    ids: string[]
-    all: Map<string,Post>
+  ids: string[];
+  all: Map<string, Post>;
+  selectedPeriod: Period;
 }
 
-export const usePosts = defineStore("posts",{
-    state: (): PostsState =>({
-        ids: [today.id, thisWeek.id, thisMonth.id],
-        all: new Map([
-            [today.id, today],
-            [thisWeek.id, thisWeek],
-            [thisMonth.id,thisMonth]
-        ])
-    }),
+export const usePosts = defineStore("posts", {
+  state: (): PostsState => ({
+    ids: [today.id, thisWeek.id, thisMonth.id],
+    all: new Map([
+      [today.id, today],
+      [thisWeek.id, thisWeek],
+      [thisMonth.id, thisMonth],
+    ]),
+    selectedPeriod: "Today",
+  }),
 
-    actions: {
+  actions: {
+    setSelectedPeriod(period: Period) {
+      this.selectedPeriod = period;
+    },
+  },
 
-    }
-})
+  getters: {
+    fillteredPosts: (state): TimeLinePost[] => {
+      return state.ids
+        .map((id) => {
+          const post = state.all.get(id)!;
+          if (!post) {
+            throw Error(`Post with id of ${id} was expected but not found.`);
+          }
+          return {
+            ...post,
+            created: DateTime.fromISO(post.created),
+          };
+        })
+        .filter((post) => {
+          if (state.selectedPeriod === "Today") {
+            return post.created >= DateTime.now().minus({ day: 1 });
+          }
 
+          if (state.selectedPeriod === "This Week") {
+            return post.created >= DateTime.now().minus({ week: 1 });
+          }
+          return post;
+        });
+    },
+  },
+});
