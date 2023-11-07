47afa61b6830263ad33ce26f8274ff1e4c9877ce
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2023-11-07 15:23:19 +0700

    Type Safety with TypeScript

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
 import { ref } from "vue"
 
-const periods = ["Today", "This Week", "This Month"]
+// DECARE TYPE
+// type Period ="Today" | "This Week" | "This Month" 
 
-const selectedPeriod = ref("Today")
+// FOR DECLATE ARRAY TYPE AS STRING ARR
+// const periods:string[] = ["A","Today", "This Week", "This Month"]
 
-function selectPeriod(period: string) {
+// For specfic type constant value of list
+//const periods = ["A","Today", "This Week", "This Month"] as const
+
+// FOR DECLARE TYPE OF periods
+const periods = ["Today", "This Week", "This Month"] as const
+type Period = typeof periods[number]
+
+// FOR PUSH NEW VALUE TO LIST
+// const periods = ["Today", "This Week", "This Month"]
+// periods.push("A")
+
+// For using specific type of Period
+const selectedPeriod = ref<Period>("Today")
+
+
+function selectPeriod(period: Period) {
     selectedPeriod.value = period
 }
 
