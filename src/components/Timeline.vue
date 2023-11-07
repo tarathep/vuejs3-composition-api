b5f309c0875a62d8c1619b755dccfd89b997d930
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2023-11-07 14:39:54 +0700

    Vue Single File Components

---
+++ src/components/Timeline.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <nav class="is-primary panel">
+        <span class="panel-tabs">
+            <a>Today</a>
+            <a>This Week</a>
+            <a>This Month</a>
+        </span>
+    </nav>
+</template>
