2eb818faba79327829a3770facd645cceb04cc70
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2023-11-07 14:52:55 +0700

    Binding wth v-bind

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -5,7 +5,22 @@ const periods = ["Today", "This Week", "This Month"]
 <template>
     <nav class="is-primary panel">
         <span class="panel-tabs">
-            <a v-for="period of periods">
+
+            <!-- can use v-bind key -->
+            <!-- <a v-for="period of periods"
+            v-bind:key="period"
+            ></a> -->
+
+            <!-- can use v-bind class -->
+            <!-- <a v-for="period of periods"
+            v-bind:class="period"
+            ></a> -->
+
+            <!-- can use shot term to use v-bind  -->
+            <a 
+            v-for="period of periods"
+            :key="period" 
+            >
                 {{ period }}
             </a>
         </span>
