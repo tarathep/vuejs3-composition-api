d21d8eef717a79debeeee66269901f3073d85fc4
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2023-11-08 16:25:34 +0700

    Creating the Post Writer Component

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { TimeLinePost } from '../posts';
+
+const props = defineProps<{
+    post: TimeLinePost
+}>()
+</script>
+
+<template>
+    <div class="columns">
+        <div class="column">
+            <div class="field">
+                <div class="label">Post title</div>
+                <input type="text" class="input">
+            </div>
+        </div>
+    </div>
+</template>
