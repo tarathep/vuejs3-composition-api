feafa0ba772e8ebff7f81a02662a478578036d6d
tarathep <tarathep.choengchaiyaphum@apac.nttdata.com>
2023-11-16 13:29:30 +0700

    v-model with custom components

---
+++ src/components/FormInput.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+defineProps<{
+    name: string
+    modelValue: string
+}>()
+
+const emit = defineEmits<{
+    (event: 'update:modelValue', value: string): void
+}>()
+
+function handleInput (e: Event) {
+    const value = (e.target as HTMLInputElement).value
+    console.log(value)
+    emit('update:modelValue',value)
+}
+</script>
+
+<template>
+    <div class="field">
+        <label for="name" class="label">{{ name }}</label>
+        <div class="control">
+            <input type="text" :id="name" class="input" :value="modelValue" @input="handleInput">
+        </div>
+    </div>
+
+</template>
